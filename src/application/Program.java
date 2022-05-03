bd414f7f739164d2693382a566990df9435bd78c
Manuel Gomes <manuel.fgj@gmail.com>
2022-05-03 13:26:22 -0300

    create Seller entity

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department dep = new Department(1,"Books");
-		System.out.println(dep);
+		Seller sel = new Seller(21, "Bob blue", "bob@gmail.com", new Date(), 3000.0, dep);
+		
+		System.out.println(sel);
 	}
 
 }
