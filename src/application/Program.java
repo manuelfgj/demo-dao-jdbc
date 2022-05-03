8ec16bf88887a1c7dc92a0cc0f82c0e4ee6e056d
Manuel Gomes <manuel.fgj@gmail.com>
2022-05-03 19:44:32 -0300

    seller delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -43,5 +43,14 @@ public class Program {
 		seller.toString();
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("\n=== TEST 6: seller delete ===");		
+		System.out.println("\nEnter id for delete test: ");
+		Scanner sc = new Scanner(System.in);
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
 }
